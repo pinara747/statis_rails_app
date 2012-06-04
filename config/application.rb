ede2bbd1ef1edb3d088159afa2de250de3b00c13
Pinar Aybar <pinaraybar@gmail.com>
2012-06-03 17:42:30 -0700

    asset pipeline precompile for heroku

--- config/application.rb
+++ config/application.rb
@@ -49,6 +49,8 @@ module StatisRailsApp
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.0'    
+    
+    config.assets.initialize_on_precompile = false
   end
 end
